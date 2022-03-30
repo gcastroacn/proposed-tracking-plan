e6b3d02dec2afb7a85f3f66be33293dffa93b9a7
Nicolina Picone <nicolina.picone@accenture.com>
2022-03-30 10:15:54 +0200

    fixed data object contents; added pageLoad event; carousel event update

--- snippet/carousel-intent.js
+++ snippet/carousel-intent.js
@@ -1,11 +1,18 @@
 // CP.8
+window.adobeDataLayer = window.adobeDataLayer || [];
 window.adobeDataLayer.push({
-    event: 'carousel-intent',
-    data: {
-        carouselTotalItem: '',
-        carouselType: '',
-        carouselDomID: '',
-        cardOrigin: '',
-        carouselIntent: ''
+   event: 'carousel-intent',
+   eventInfo :{
+       "eventName" :"carousel-intent"
     },
+    component: {
+        carousel: {
+            "carouselTotalItem": 1,
+            "contentType":"",
+            "carouselIntent": "",
+            "contentOrigin": "",
+            "componentType": "",
+            "componentName": ""
+        }
+    }
 });
---
+++ snippet/pageLoad.js
@@ -0,0 +1,39 @@
+window.adobeDataLayer = window.adobeDataLayer || [];
+window.adobeDataLayer.push({
+    event: 'pageLoad',
+    eventInfo :{
+        "eventName" :"page loaded"
+     },
+    page: {
+        pageInfo: {
+           "pageName": "",
+           "siteSection": "",
+           "languageURL": "",
+           "siteSectionLevel2": "",
+           "siteSectionLevel3": "",
+           "siteSectionLevel4": "",
+           "siteSectionLevel5": "",
+           "siteSectionLevel6": "",
+           "pageNotFound": "",
+           "currency": "",
+           "internalReferrer": "",
+           "pagePathname": "",
+           "pageHostname": "",
+           "pageLoadingTime": "",
+           "environment": "",
+           "digitalProperty": ""
+        },
+        attributes: {
+           "contentType": "",
+           "destinationRelatedList": [],
+           "isFavourite":"",
+           "contentTag": [],
+           "contentAuthor": "",
+           "contentID": "",
+           "contentRead": "",
+           "estimatedReadTime": "",
+           "tagTitle": "",
+           "contentTitle": ""
+        }
+     }
+  });    
