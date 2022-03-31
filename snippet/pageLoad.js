a55a43a3c0e2124f97aba10c5efbeafccc6c8e74
Nicolina Picone <nicolina.picone@accenture.com>
2022-03-31 16:21:55 +0200

    update page load

--- snippet/pageLoad.js
+++ snippet/pageLoad.js
@@ -1,39 +1,20 @@
-window.adobeDataLayer = window.adobeDataLayer || [];
 window.adobeDataLayer.push({
-    event: 'pageLoad',
+    event: 'page-loaded',
     eventInfo :{
-        "eventName" :"page loaded"
+        "eventName" :"page-loaded"
      },
     page: {
         pageInfo: {
-           "pageName": "",
-           "siteSection": "",
-           "languageURL": "",
-           "siteSectionLevel2": "",
-           "siteSectionLevel3": "",
-           "siteSectionLevel4": "",
-           "siteSectionLevel5": "",
-           "siteSectionLevel6": "",
-           "pageNotFound": "",
-           "currency": "",
-           "internalReferrer": "",
-           "pagePathname": "",
-           "pageHostname": "",
-           "pageLoadingTime": "",
-           "environment": "",
-           "digitalProperty": ""
-        },
-        attributes: {
-           "contentType": "",
-           "destinationRelatedList": [],
-           "isFavourite":"",
-           "contentTag": [],
-           "contentAuthor": "",
-           "contentID": "",
-           "contentRead": "",
-           "estimatedReadTime": "",
-           "tagTitle": "",
-           "contentTitle": ""
+           "pageName": ""
         }
-     }
+     },
+     userProfile: {
+      "UID": "",
+      "loginStatus": ""
+     },
+     thirdPartyService :{
+      "method": "",
+      "callID": "",
+      "callError": ""
+   }
   });    
